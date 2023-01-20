bfc1a393ae6ceb090a61577a76da3728f65a5f5b
Lily Springer <lilyspringer@hotmail.com>
2023-01-19 19:45:26 -0700

    problems solved thru 9

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -59,10 +59,22 @@ var score = 74
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, c
+  onsole log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 
+  'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+if (score >= 90) {
+  console.log('You got an A!')
+} else if (score >= 80) {
+  console.log('You got a B!')
+} else if (score >= 70) {
+  console.log('You got a C')
+} else if (score >= 60) {
+  console.log('You got a D')
+} else {
+  console.log('You got an F, both as your grade and in the chat')
+}
 
 
 ////////// Intermediate Problems //////////
@@ -77,16 +89,25 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
   Create a variable named 'someNum' and set it equal to the fifth value of the 'myFavoriteNumbers' array.
 */
 
-//Code Here
+let someNum = myFavoriteNumbers[4]
+console.log(someNum)
 
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+/* Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, 
+but there are not 7 elements in the array? Let's write some code to check for that. 
+ Use an if statement to check the length of the 'myFavoriteNumbers' array. 
+ If it's less than 7, log 'There are not enough elements in this array' to the console. 
+ If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. 
+ (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+*/
 
-//Code Here
-
+if (myFavoriteNumbers.length < 7) {
+  console.log('There are not enough elements in this array.')
+} else {
+  someNum = myFavoriteNumbers[6]
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -94,9 +115,15 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. 
+//If it is, console.log '{number} is divisible by 3.'
 
-//Code Here
+for (let i = 0; i < listOfNumbers.length; i++) {
+  if (listOfNumbers[i] % 3 === 0) {
+    console.log(`${listOfNumbers[i]} is divisible by 3.`)
+  }
+}
+
 
 
 ////////// PROBLEM 9 //////////
@@ -108,7 +135,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
 */
 
-//Code Here
+for (let i = letters.length - 1; i >= 0; i--) {
+  console.log(letters[i])
+}
 
 
 ////////// Advanced Problems //////////
